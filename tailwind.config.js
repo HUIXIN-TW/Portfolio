cddb853ff1024d93d82d20a5c5b180ce1428a84d
HUIXIN-TW <huixin.yang.tw@gmail.com>
2024-01-28 23:52:53 +0800

    Updates

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  // Other Tailwind CSS configurations...
-
-  // Configure the content option to analyze HTML and JSX files
-  content: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.jsx"],
-};
