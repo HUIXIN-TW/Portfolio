40343574c3cb777bc9463bb40b3a1804960763da
HUIXIN-TW <huixin.yang.tw@gmail.com>
2024-02-04 02:08:57 +0800

    add button

--- src/Components/common/Button.jsx
+++ src/Components/common/Button.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import "../../styles/components/Button.css";
 
 const Button = ({ onClick, text }) => {
-  return (
-    <button className="small-button" onClick={onClick}>
-      {text}
-    </button>
-  );
+  return <button onClick={onClick}>{text}</button>;
 };
 
 export default Button;
