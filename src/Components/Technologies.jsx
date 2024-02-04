0089ffe2b35660b13e3c526bb2e553f43ebbc0c6
HUIXIN-TW <huixin.yang.tw@gmail.com>
2024-02-05 00:53:12 +0800

    fix interaction

--- src/Components/Technologies.jsx
+++ src/Components/Technologies.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 
 const Technologies = ({ technologies, setFilter }) => {
+  const handleKeyPress = (event, tech) => {
+    if (event.key === "Enter" || event.key === " ") {
+      setFilter(tech);
+    }
+  };
+
   return (
     <div className="technologies-container">
       {technologies.map((tech, index) => (
         <span
+          role="button"
           key={index}
           className="technology-tag" // Add some CSS class for styling
           onClick={() => setFilter(tech)} // Set the filter to the clicked technology
+          tabIndex="0"
+          onKeyDown={(event) => handleKeyPress(event, tech)} // Handle keyboard interactions
         >
           {tech}
         </span>
