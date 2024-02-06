709d76018097d69e0ee5540030afb9f37fc870be
HUIXIN-TW <huixin.yang.tw@gmail.com>
2024-02-07 03:12:17 +0800

    fix the layout of all tags

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -16,7 +16,7 @@ const Portfolio = () => {
   return (
     <section className="padding" id="portfolio">
       <h2 className="text-center">Work & Project Gallery</h2>
-      <div className="flex justify-center mt-5 mr-20 ml-20 mb-5 p-3">
+      <div className="flex justify-center text-xs lg:mt-5 lg:mr-5 lg:ml-5 lg:mb-0 p-3">
         <TechnologiesButton onTechnologyClick={handleTechnologyClick} />
       </div>
       <div className="pictureContainer flex flex-row p-2 lg:pt-12 lg:item-center">
