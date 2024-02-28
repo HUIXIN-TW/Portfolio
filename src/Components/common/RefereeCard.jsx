3bb55b39abfcce341f7755a13ca7236bbcdc05d4
HUIXIN-TW <huixin.yang.tw@gmail.com>
2024-02-28 17:10:09 +0800

    run format

--- src/Components/common/RefereeCard.jsx
+++ src/Components/common/RefereeCard.jsx
@@ -17,7 +17,13 @@ const RefereePeriod = styled.span`
   font-style: italic;
 `;
 
-const RefereeCard = ({ refereeName, refereeType, currentPosition, relationship, periodWorking }) => {
+const RefereeCard = ({
+  refereeName,
+  refereeType,
+  currentPosition,
+  relationship,
+  periodWorking,
+}) => {
   return (
     <div className="bg-white shadow-md rounded-lg p-6 mb-4">
       <RefereeHeader>
