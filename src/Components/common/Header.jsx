5763123d80747589cd75273f514f1bb022026bd5
HUIXIN-TW <huixin.yang.tw@gmail.com>
2025-01-30 01:58:19 +0800

    feat: add avatar

--- src/Components/common/Header.jsx
+++ src/Components/common/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import photo from "../../assets/images/photo.jpg";
+import photo from "../../assets/images/avatar.webp";
 import GitHubButton from "./GitHubButton";
 
 const Header = () => {
